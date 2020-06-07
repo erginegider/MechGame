6afcf6f40e1997a2b923021289db7051b03b7e34
ergin <erginduzu@gmail.com>
2020-06-07 08:47:54 +0300

    code: Missile complete without atenuation

--- Source/MechGame/Missile.cpp
+++ Source/MechGame/Missile.cpp
@@ -9,7 +9,8 @@
 #include "GameplayEffect.h"
 #include "MechGameCharacter.h"
 #include "AbilitySystemComponent.h"
-
+#include "Kismet/GameplayStatics.h"
+#include "Sound/SoundCue.h"
 // Sets default values
 AMissile::AMissile()
 {
@@ -34,9 +35,10 @@ AMissile::AMissile()
 	DamageToApply = 1.0f;
 
 	CollissionCapsule->IgnoreActorWhenMoving(this->GetOwner(), true);
-	//CollissionCapsule->SetCollisionProfileName()
+	
 	CollissionCapsule->OnComponentBeginOverlap.AddDynamic(this,&AMissile::OnCapsuleBeginOverlap);
 	
+	CollissionCapsule->SetMassOverrideInKg(FName::FName(), 0.0f, true);
 }
 
 // Called when the game starts or when spawned
@@ -45,35 +47,64 @@ void AMissile::BeginPlay()
 	Super::BeginPlay();
 	
 }
+
+
+
+
 void AMissile::OnCapsuleBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
 {
+	AMechGameCharacter *OwnerCharacter = Cast<AMechGameCharacter>(UGameplayStatics::GetPlayerPawn(GetWorld(), 0));	
+	if (OwnerCharacter == nullptr)
+		return;
+
+	FTransform SpawnTransform;
+	SpawnTransform.SetScale3D(FVector(5.0f, 5.0f, 5.0f));
+	
 	if (OtherActor && OtherActor != this && OtherComp)
 	{
 
-		AMechGameCharacter *OtherCharacter = Cast<AMechGameCharacter>(OtherActor);
-		if (OtherCharacter)
-		{
-			UAbilitySystemComponent *OtherAbilitySystemComponent = OtherCharacter->GetAbilitySystemComponent();
-			if (OtherAbilitySystemComponent)
-			{
-				if (DamageEffect)
-				{
-					FGameplayEffectContextHandle Context = OtherAbilitySystemComponent->MakeEffectContext();
-					Context.AddHitResult(SweepResult);
-					Context.AddOrigin(SweepResult.Location);
-					TArray<TWeakObjectPtr<AActor>> ActorList;
-					ActorList.Add(OtherActor);
-					Context.AddActors(ActorList);
-					Context.AddSourceObject(this);
-					FGameplayEffectSpecHandle DamageEffectSpecHandle= OtherAbilitySystemComponent->MakeOutgoingSpec(DamageEffect, 1.0, Context);
-					
-					OtherAbilitySystemComponent->ApplyGameplayEffectSpecToSelf(*DamageEffectSpecHandle.Data.Get());
-				}
-			}
+			AMechGameCharacter *OtherCharacter = Cast<AMechGameCharacter>(OtherActor);
 
-		}
-		
+			if (OtherCharacter && OtherCharacter!=OwnerCharacter) 
+			{			
+				
+					UAbilitySystemComponent *OtherAbilitySystemComponent = OtherCharacter->GetAbilitySystemComponent();
+					if (OtherAbilitySystemComponent)
+					{
+						if (DamageEffect)
+						{
+							FGameplayEffectContextHandle Context = OtherAbilitySystemComponent->MakeEffectContext();
+							FVector Location = GetActorLocation();
+
+							Context.AddOrigin(Location);
+						
+							TArray<TWeakObjectPtr<AActor>> ActorList;
+							ActorList.Add(OtherActor);
+							Context.AddActors(ActorList);
+							Context.AddSourceObject(OtherActor);
+							FGameplayEffectSpecHandle DamageEffectSpecHandle = OtherAbilitySystemComponent->MakeOutgoingSpec(DamageEffect, 1.0, Context);
+							SpawnTransform.SetTranslation(GetActorLocation());
+							OtherAbilitySystemComponent->ApplyGameplayEffectSpecToSelf(*DamageEffectSpecHandle.Data.Get());
+							
+							UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), FireParticle, SpawnTransform);
+							UGameplayStatics::SpawnSoundAtLocation(GetWorld(), FireSound, GetActorLocation());
+							this->Destroy(); return;
+						}
+					}
+				
+			}
+			else if(this->GetClass()!=OtherActor->GetClass() &&  OtherCharacter!=OwnerCharacter)
+			{
+
+				
+				SpawnTransform.SetTranslation(GetActorLocation());
+				UGameplayStatics::SpawnSoundAtLocation(GetWorld(), FireSound, SpawnTransform.GetLocation());
+				UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), FireParticle, SpawnTransform);
+				this->Destroy(); return;
+			}
+	
 	}
+	
 	
 }
 
--- Source/MechGame/Missile.h
+++ Source/MechGame/Missile.h
@@ -19,6 +19,8 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+
+	
 	UFUNCTION()
 	void OnCapsuleBeginOverlap(UPrimitiveComponent * OverlappedComponent, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult);
 
@@ -35,7 +37,12 @@ protected:
 	class UArrowComponent   *ArrowComponent;
 
 public:
+	UPROPERTY(EditAnyWhere, BlueprintReadWrite, Category = Campfire)
+	UParticleSystem* FireParticle;
 
+
+	UPROPERTY(EditAnyWhere, BlueprintReadWrite, Category = Campfire)
+	class USoundCue* FireSound;
 
 	UPROPERTY(EditDefaultsOnly,Category = "Projectile")
 	TSubclassOf<class UGameplayEffect> DamageEffect;
